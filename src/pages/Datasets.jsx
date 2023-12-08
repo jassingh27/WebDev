4949e5f3f3be86605d05772aa90b28b64312cd50
Thisu <s222084424@deakin.edu.au>
2023-12-09 01:11:50 +1100

    data set page with hovering effect on the data sets by thisu

--- src/pages/Datasets.jsx
+++ src/pages/Datasets.jsx
@@ -7,8 +7,67 @@ const Datasets = () => {
     <div>
       <Header />
       <main>
-        {/* Your home page content goes here */}
-        <h2>Welcome to the Datasets Page!</h2>
+        
+<div class="header">
+ <h2>What are you Looking for Today?</h2>
+</div>
+
+<div class="container">
+ <div class="section ">
+    
+    <img src="images/people.png" alt="Property"/>
+    <h3>People</h3>
+ </div>
+
+ <div class="section">
+   
+ </div>
+
+ <div class="section">
+    
+    <img src="images/cloud.png" alt="Transportation"/>
+    <h3>Sensors</h3>
+ </div>
+
+ <div class="clearfix"></div>
+</div>
+<div class="container">
+    <div class="section">
+       
+       
+    </div>
+   
+    <div class="section">
+       
+       <img src="images/recycle.png" alt="Environment"/>
+       <h3>Environment</h3>
+    </div>
+   
+    <div class="section">
+      
+    </div>
+   
+    <div class="clearfix"></div>
+   </div>
+   <div class="container">
+    <div class="section">
+       
+       <img src="images/house.png" alt="Property"/>
+       <h3>Property</h3>
+    </div>
+   
+    <div class="section">
+       
+    </div>
+   
+    <div class="section">
+      
+       <img src="images/tram.png" alt="Transportation"/>
+       <h3>Transportation</h3>
+    </div>
+   
+    <div class="clearfix"></div>
+   </div>
       </main>
       <Footer />
     </div>
