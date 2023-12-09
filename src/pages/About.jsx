1a44e92e87888c53ec16fa48b2d702ae18d32987
Kasey <kaseywu130@gmail.com>
2023-12-10 10:18:42 +1100

    Fixed a file pathway issue
    Fixed the pathway to CSS file so that the website can show properly.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '.src/styles/about.css';
+import '../styles/about.css';
 
 const About = () => {
   return (
